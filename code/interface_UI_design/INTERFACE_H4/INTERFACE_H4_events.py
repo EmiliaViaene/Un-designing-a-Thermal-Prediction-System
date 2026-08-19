4471dd9bd62024f2a287174eaf22f411c81c6980
Emilia Viaene <104364594+EmiliaViaene@users.noreply.github.com>
2026-08-19 21:16:54 +0200

    Add files via upload

---
+++ code/interface_UI_design/INTERFACE_H4/INTERFACE_H4_events.py
@@ -0,0 +1,28 @@
+
+def gisteren_setdatelabels(event_struct):
+    return
+
+
+def gisteren_setvalues_1(event_struct):
+    return
+
+
+def gisteren_setvalues_2(event_struct):
+    return
+
+
+def gisteren_setvalues_3(event_struct):
+    return
+
+
+def gisteren_setvalues_4(event_struct):
+    return
+
+
+def SETTINGS_SETMAX(event_struct):
+    return
+
+
+def SETTINGS_SETMIN(event_struct):
+    return
+
